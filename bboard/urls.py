9db9b379ba89e2b86b3e65f7517cb37c096a65bc
AliyaNurmakhanova <aliyajeon91@gmail.com>
2023-03-06 16:20:37 +0600

    login, main, forgot pages

---
+++ bboard/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, re_path
+from bboard import views
+
+urlpatterns = [
+    path('', views.login_page, name='login'),
+    path('/index/', views.index, name='index'),
+    path('/forgot/', views.forgot_pw, name='forgot'),
+]
--- bboard/views.py
+++ bboard/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def login_page(request):
+    return render(request, 'login_page.html')
+
+def index(request):
+    return render(request, 'index.html')
+
+def basic(request):
+    return render(request, 'basic.html')
+
+def forgot_pw(request):
+    return render(request, 'forgot_pw.html')
