4edcbfa1c9d60e3b9c291f025d45e8e5d006dee6
zhanerkeakimzhan <74965537+Zhanerke2506@users.noreply.github.com>
2023-03-26 16:24:35 +0600

    doublef*ck

--- bboard/models.py
+++ bboard/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class Students(models.Model):
-    name = models.CharField('Имя', max_length=50)
-    lastname = models.CharField('Фамилия', max_length=50)
-    middlename = models.CharField('Отчество', max_length=50)
+    name_kz = models.CharField('Имя', max_length=50)
+    lastname_kz = models.CharField('Фамилия', max_length=50)
+    middlename_kz = models.CharField('Отчество', max_length=50)
     birthday = models.DateField('День рождения')
-    stud_id = models.IntegerField('ИИН', max_length=12)
     diploma_title = models.CharField('Тема дипломного проекта', max_length=100)
+    stud_id = models.IntegerField('ИИН', max_length=12)
 
     def __str__(self):
-        return self.name
+        return self.name_kz
 
     class Meta:
         verbose_name = 'Студенты'
--- bboard/views.py
+++ bboard/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Students
 from docxtpl import DocxTemplate
 
+
 def login_page(request):
     return render(request, 'login_page.html')
 
@@ -18,18 +19,38 @@ def edit_stud_page(request):
 def forgot_pw(request):
     return render(request, 'forgot_pw.html')
 
-<<<<<<< HEAD
 def documents(request):
     students = Students.objects.all()
     return render(request, 'document_page.html', {'students': students})
+    def example():
+        personNames = ["Aliya", "Samal", "Safia", "Kamshat"]
 
-=======
+        for pItr, p in enumerate(personNames):
+            doc = DocxTemplate("inviteTmpl.docx")
+
+            context = {
+                "todayStr": "03.03.2023",
+                "recipientName": p,
+                "evntDtStr": "08.03.2023",
+                "venueStr": "the beach",
+                "bannerImg": ""
+            }
+
+            doc.render(context)
+
+            doc.save("aaaa_{0}.docx".format(p))
+
+def documents_second(request):
+    students = Students.objects.all()
+    return render(request, 'document_page_second.html', {'students': students})
+
+def documents_third(request):
+    students = Students.objects.all()
+    return render(request, 'document_page_third.html', {'students': students})
+
 def students(request):
-    return render(request, 'students.html')
+    stud = Students.objects.all()
+    return render(request, 'students.html', {'stud': stud})
 
 def commissions(request):
     return render(request, 'commissions.html')
-
-def documents(request):
-    return render(request, 'documents.html')
->>>>>>> 2944f1b15c565ef153aaa3a248579e136977aa67
