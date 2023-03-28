be265ca3fb340b663ae230c86b5e2ba1f72b25a2
zhanerkeakimzhan <74965537+Zhanerke2506@users.noreply.github.com>
2023-03-29 01:18:31 +0600

    newDB

--- bboard/models.py
+++ bboard/models.py
@@ -7,10 +7,16 @@ class Students(models.Model):
     middlename = models.CharField('Отчество', max_length=50)
     birthday = models.DateField('День рождения')
     diploma_title = models.CharField('Тема дипломного проекта', max_length=100)
-    iin = models.CharField('ИИН', max_length=100, default='ИИН')
-    # name_eng = models.CharField('Имя на английском', max_length=100, default='Имя')
-    # lastname_eng = models.CharField('Фамилия на английском', max_length=100, default='Фамилия')
-    # middlename_eng = models.CharField('Отчество на английском', max_length=100, default='Отчество')
+    iin = models.CharField('ИИН', max_length=100, default=' ')
+    name_eng = models.CharField('Имя на английском', max_length=100, default=' ')
+    lastname_eng = models.CharField('Фамилия на английском', max_length=100, default=' ')
+    middlename_eng = models.CharField('Отчество на английском', max_length=100, default=' ')
+    id_card = models.CharField('№ удостоверения личности/паспорта', max_length=100, default=' ')
+    speciality = models.CharField('Специальность', max_length=100, default=' ')
+    prez_diploma = models.FileField('Презентация', upload_to='uploads/')
+    recen_diploma = models.FileField('Рецензия', upload_to='uploads/')
+    feedback_diploma = models.FileField('Отзыв руководителя', upload_to='uploads/')
+    antiplagiat = models.FileField('Антиплагиат', upload_to='uploads/')
 
     def __str__(self):
         return self.name
